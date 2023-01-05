067a552b1dc6f045ff0a78654bdcc6d09d8af737
Bogdan Pop <pop.bogdan.pvab@gmail.com>
2023-01-06 00:15:07 +0100

    feat(sol): Daily challenge 452

--- src/solutions/find_min_arrow_shots_452.rs
+++ src/solutions/find_min_arrow_shots_452.rs
@@ -11,7 +11,7 @@
 //! Given the array points, return the minimum number of arrows that must be shot to burst all
 //! balloons.
 //!
-//!  
+//!
 //!
 //! Example 1:
 //!
@@ -35,7 +35,7 @@
 //! - Shoot an arrow at x = 2, bursting the balloons [1,2] and [2,3].
 //! - Shoot an arrow at x = 4, bursting the balloons [3,4] and [4,5].
 //!
-//!  
+//!
 //!
 //! Constraints:
 //!
@@ -45,6 +45,8 @@
 //!
 
 mod find_min_arrow_shots_452 {
+    use std::i32;
+
     /// In order to find the minimum arrow shots, we need to proceed with the following steps:
     /// 1. Order the points in increasing order by the starting points.
     /// 2. Scan the points from the start and save the smallest ending point of the balloons
@@ -54,9 +56,27 @@ mod find_min_arrow_shots_452 {
     /// 4. Start again with the starting point that is higher than the smallest starting point
     ///    where you just shot.
     /// 5. Repeat 2,3 and 4 util the end of the balloons.
-    
-    pub fn find_min_arrow_shots(points: Vec<Vec<i32>>) -> i32 {
 
+    pub fn find_min_arrow_shots(points: Vec<Vec<i32>>) -> i32 {
+        let mut balloons = points;
+        let mut arrows = 0;
+        let mut min_end = std::i32::MAX;
+        // let mut flag: bool = false;
+        // 1. Order the balloons
+        balloons.sort_by_key(|p| p[0]);
+        // println!("Sorted balloons: {:#?}", balloons);
+
+        for b in balloons {
+            if b[0] > min_end {
+                arrows += 1;
+                min_end = b[1];
+            } else if min_end > b[1] {
+                min_end = b[1];
+            }
+        }
+
+        arrows += 1;
+        arrows
     }
 }
 
@@ -64,25 +84,47 @@ mod find_min_arrow_shots_452 {
 mod tests {
     use super::find_min_arrow_shots_452::find_min_arrow_shots;
 
-
     #[test]
-    fn  test_example_1() {
+    fn test_example_1() {
         let points = vec![vec![10, 16], vec![2, 8], vec![1, 6], vec![7, 12]];
 
         assert_eq!(2, find_min_arrow_shots(points));
     }
-    
+
     #[test]
-    fn  test_example_2() {
+    fn test_example_2() {
         let points = vec![vec![1, 2], vec![3, 4], vec![5, 6], vec![7, 8]];
 
         assert_eq!(4, find_min_arrow_shots(points));
     }
 
     #[test]
-    fn  test_example_3() {
+    fn test_example_3() {
         let points = vec![vec![1, 2], vec![2, 3], vec![3, 4], vec![4, 5]];
 
-        assert_eq!(4, find_min_arrow_shots(points));
+        assert_eq!(2, find_min_arrow_shots(points));
+    }
+
+    #[test]
+    fn test_example_4() {
+        let points = vec![vec![-1, 1], vec![0, 1], vec![2, 3], vec![1, 2]];
+
+        assert_eq!(2, find_min_arrow_shots(points));
+    }
+
+    #[test]
+    fn test_example_5() {
+        let points = vec![
+            vec![4, 12],
+            vec![7, 8],
+            vec![7, 9],
+            vec![7, 9],
+            vec![2, 8],
+            vec![6, 7],
+            vec![5, 14],
+            vec![4, 13],
+        ];
+
+        assert_eq!(1, find_min_arrow_shots(points));
     }
 }
