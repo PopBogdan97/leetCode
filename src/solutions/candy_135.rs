6121bddc0afff222d2eb6d7c2af325c8a7331b59
Bogdan Pop <pop.bogdan.pvab@gmail.com>
2023-01-08 23:41:49 +0100

    feat(sol): Challege 135 solution

--- src/solutions/candy_135.rs
+++ src/solutions/candy_135.rs
@@ -36,7 +36,26 @@
 //!
 
 mod candy_135 {
+    /// In order to complete the task it is enough to scan two times the ratings array forward and
+    /// backwards and always check that the child in the ratings array you are taking into account
+    /// has 1 more candy that the one before if its rating is better. 
     pub fn candy(ratings: Vec<i32>) -> i32 {
+        let mut candies: Vec<i32> = vec![1; ratings.len()];
+
+        for i in 1..ratings.len() {
+            if ratings[i] > ratings[i - 1] {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for i in (0..(ratings.len() - 1)).rev() {
+            if ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1] {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+
+        let sum: i32 = candies.iter().sum();
+        sum
     }
 }
 
@@ -56,5 +75,19 @@ mod tests {
         let ratings = vec![1, 2, 2];
 
         assert_eq!(4, candy(ratings));
+    }
+
+    #[test]
+    fn test_example_3() {
+        let ratings = vec![1, 2, 87, 87, 87, 2, 1];
+
+        assert_eq!(13, candy(ratings));
+    }
+
+    #[test]
+    fn test_example_4() {
+        let ratings = vec![1, 3, 4, 5, 2];
+
+        assert_eq!(11, candy(ratings));
     }
 }
